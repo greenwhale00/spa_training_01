c4c129860f8b1cb536d2b0abd379b2821049de2c
greenwhale00 <goreakay@gmail.com>
2022-09-22 15:55:36 +0900

    220922

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,10 +4,16 @@ import MainContent from './MainContent';
 import Customer from './Customer';
 
 const Main = () => {
+    const MAINSLIDER = [
+        { id: 1, tit: "아파트입주청소", con: "신축 아파트나 빌라 등 입주 전 상태에서의 실내 청소", des: "구석구석 세밀하게 청소하는 서비스로 가족의 건강을 위해서 입주 전에 꼭 해야 하는 서비스입니다.", link: "/sub02" },
+        { id: 2, tit: "이사/상가청소", con: "오랫동안 누적된 공간의 묵은 때, 찌든 때, 누적된 먼지와 세균들을 이사하기전에 청소", des: "신축 건물은 공사기간 중 쌓인 미세먼지/유해성분들이 곳곳에 산재해 있습니다. 입추청소를 맡기시면 유해성분 제거하고 청결한 환경을 만드실 수 있습니다.", link: "/sub03" },
+        { id: 3, tit: "사무실청소", con: "쾌적한 업무 환경을 만들고, 능률적인 업무를 수행하기 위해 반드시 필요한 청소", des: "쾌적한 업무 환경을 만들고, 능률적인 업무를 수행하기 위해 반드시 필요한 청소 작업 입니다. 사무실청소에 있어서 가장 중요한 점은 빠른 시간 내에 완벽한 작업을 마무리 하는 것이 중요합니다.", link: "/sub04" },
+    ];
+
     return (
         <main className='Main'>
             <MainVisual />
-            <MainContent />
+            <MainContent word={MAINSLIDER} />
             <Customer />
         </main>
     )
--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const MainContent = () => {
+const MainContent = ({ word }) => {
     return (
-        <div>MainContent</div>
+        <section className='MainContent'>
+            <div className="inner">
+                {
+                    word.map((con, idx) => {
+                        return (
+                            <Link to={con.link}>
+                                <figure key={con.id} >
+                                    <img src={process.env.PUBLIC_URL + `/assetes/imges/main_s01${idx + 1}.jpg`} alt="" />
+                                </figure>
+                                <strong>{con.tit}</strong>
+                                <p>
+                                    {con.des}
+                                </p>
+                            </Link>
+                        )
+                    })
+
+                }
+            </div>
+        </section >
     )
 }
 
